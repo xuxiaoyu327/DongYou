8dcbb469f00e91a00cc8d41c300e1ec736acf771
许小雨 <3487776176@qq.com>
2025-12-04 21:36:35 +0800

    feat: add TypeScript support for .vue files and include Element Plus types

--- project/env.d.ts
+++ project/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+/// <reference types="element-plus/global" />
+
+// 让 TypeScript 认识以 .vue 结尾的单文件组件，避免“找不到模块声明”的报错
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
